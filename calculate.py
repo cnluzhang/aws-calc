79147b54aca75793b2d9297871a16d246a3ecb69
Sean Chang <git@lu.email>
2021-08-17 14:10:15 +0800

    Add more size

--- calculate.py
+++ calculate.py
@@ -1,8 +1,8 @@
 import boto3
 import sys
 
-num_dic = {"nano": 0.25, "micro": 0.5, "small": 1, "medium": 2, "large": 4, "xlarge": 8, "2xlarge": 16, "4xlarge": 32,
-           "8xlarge": 64, "12xlarge": 96, "16xlarge": 128, "18xlarge": 144, "24xlarge": 192}
+num_dic = {"nano": 0.25, "micro": 0.5, "small": 1, "medium": 2, "large": 4, "xlarge": 8, "2xlarge": 16, "3xlarge": 24, "4xlarge": 32, "6xlarge": 48,
+           "8xlarge": 64, "9xlarge": 72, "12xlarge": 96, "16xlarge": 128, "18xlarge": 144, "24xlarge": 192}
 
 
 class Resource(object):
